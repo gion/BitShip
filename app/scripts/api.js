964c5ffce128ad660e6d3d6df897ec713ff9cf7a
gion <bogdan.gradinariu@thinslices.com>
2015-07-09 16:45:49 +0300

    used jQuery for dom manipulation, ajax and defer fn
    and get all the projects from the codeship endpoint

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,4 +1,5 @@
 var util = require('./util.js');
+var $ = require('./jquery-2.1.4.min.js');
 
 var api = (function() {
   var api = {
@@ -20,43 +21,58 @@ var api = (function() {
       return branch;
     },
 
+    getCodeshipProjects: function(codeshipAPiKey, success, fail) {
+      codeshipAPiKey = codeshipAPiKey || api.settings.codeship.apiKey;
+
+      var url = 'https://codeship.com/api/v1/projects.json?api_key=' + codeshipAPiKey;
+      return $.get(url).done(success).fail(fail);
+    },
+
     _getCodeshipStatus: function(codeshipAPiKey, codeshipProjectId, success, fail) {
       var url = 'https://codeship.com/api/v1/projects/'+ codeshipProjectId +'.jsonp?api_key=' + codeshipAPiKey;
-      // console.log('_getCodeshipStatus url', url);
-
-      util.ajax('get', url, success, fail);
+      return $.get(url).done(success).fail(fail);
     },
 
     _getCodeshipStatusForBranch: function(branchName, codeshipAPiKey, codeshipProjectId, success, fail) {
-      api._getCodeshipStatus(codeshipAPiKey, codeshipProjectId, function(response) {
-        // console.log('_getCodeshipStatus', response);
-        var build = response.builds.filter(function(build) {
-          return build.branch === branchName;
-        })[0];
+      var defer = $.Deferred();
 
-        success(build.status, build);
-      }, fail);
+      api._getCodeshipStatus(codeshipAPiKey, codeshipProjectId)
+        .done(function(response) {
+          var build = response.builds.filter(function(build) {
+            return build.branch === branchName;
+          })[0];
+
+          defer.resolve(build.status, build);
+
+          if(success) {
+            success(build.status, build);
+          }
+        })
+        .fail(fail);
+
+        return defer.promise();
     },
 
-    getBuildStatus: function(success, fail) {
+    getBuildStatus: function(project) {
       var branchName = api.getBranchName(),
-          codeshipProjectId = '89807',
-          codeshipAPiKey = '3c3334406595e60ebe377306ce202bf91c9db509ab0a928e2858f1f706d1';
+          codeshipProjectId = project.id,
           codeshipAPiKey = api.settings.codeship.apiKey;
 
-      api._getCodeshipStatusForBranch(branchName, codeshipAPiKey, codeshipProjectId, success, fail);
+      return api._getCodeshipStatusForBranch(branchName, codeshipAPiKey, codeshipProjectId);
     },
 
-    updateUI: function(status) {
-      util.removeClass('html', 'bitship-status-error bitship-status-success bitship-status-testing');
-      util.addClass('html', 'bitship-status-' + status);
-      util.removeElement('#bitship-container');
-      api._addNotificationDom();
+    updateUI: function(status, buildData) {
+      $('html')
+        .removeClass('bitship-status-error bitship-status-success bitship-status-testing')
+        .addClass('bitship-status-' + status);
+
+      $('#bitship-container').remove();
+      api._addNotificationDom(buildData);
     },
 
-    _addNotificationDom: function() {
-      var domEl = document.createDocumentFragment(),
-          container = document.createElement('section'),
+    _addNotificationDom: function(buildData) {
+      var //domEl = document.createDocumentFragment(),
+          container = document.createElement('a'),
           message = document.createElement('span'),
           statusImg = document.createElement('img'),
           codeshipUUID = 'c8787840-06ba-0133-1ccd-2aa9a23a545f',
@@ -70,25 +86,53 @@ var api = (function() {
       message.appendChild(statusImg);
 
       container.setAttribute('id', 'bitship-container');
+      container.setAttribute('href', 'https://codeship.com/projects/'+ buildData.project_id +'/builds/' + buildData.id);
+      container.setAttribute('target', '_blank');
+      container.setAttribute('title', 'Open the codeship build page');
       container.appendChild(message);
 
-      domEl.appendChild(container)
-      util.insertAfter(domEl, document.querySelector('#pull-request-diff-header'));
+      // domEl.appendChild(container)
+      var target = $('#pull-request-diff-header');
+      $(container).insertAfter(target);
+    },
+
+    getBitbucketProjectFromUrl: function(url) {
+      return (url || location.toString()).replace(/^.*bitbucket\.org\/(.*?)\/pull-request.*$/, '$1').toLowerCase();
+    },
+
+    isEnabledForProject: function(projectData) {
+      if(!projectData.repository_name) {
+        return false;
+      }
+
+      var isSameName = projectData.repository_name.toLowerCase() === api.getBitbucketProjectFromUrl(),
+          isBitbucketProject = projectData.repository_provider.toLowerCase() === 'bitbucket';
+
+      return isBitbucketProject && isSameName;
     },
 
     run: function() {
 
+      $('html').addClass('bitship-enabled');
+
       api.settings = util.storage.get(function(settings) {
         api.settings = settings;
 
-        console.log(api.settings);
+        api.getCodeshipProjects()
+          .done(function(response) {
+            api.projects = response.projects;
 
-        util.addClass('html', 'bitship-enabled');
-        api.getBuildStatus(function success(status, buildData) {
-          api.updateUI(status);
-        }, function error() {console.warn('nasol', arguments);});
+            api.projects
+            .filter(api.isEnabledForProject)
+            .forEach(function(project) {
+              api.getBuildStatus(project)
+                .done(api.updateUI);
+            });
+          })
+          .fail(function() {
+            $('html').removeClass('bitship-enabled');
+          });
       });
-
     }
   };
 
