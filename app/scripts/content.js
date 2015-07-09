6c94d597bfe8e2a101782f8923279216dd33e17a
gion <bogdan.gradinariu@thinslices.com>
2015-07-09 16:47:17 +0300

    removed dummy hardcoded settings

--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,9 +1,3 @@
 'use strict';
 
-var util = require('./util.js');
-var api = require('./api.js');
-
-// this shuold be removed on "production"
-util.storage.save(require('./settings.json'), function() {
-  api.run();
-});
+require('./api.js').run();
