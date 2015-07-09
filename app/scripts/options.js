1b1ef1dc1fd0b611778925297193e313c9b3ce35
gion <bogdan.gradinariu@thinslices.com>
2015-07-09 10:55:56 +0300

    first try for an angular-based options html page

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,4 +1,16 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
-alert('options');
+var util = require('./util.js');
+var api = require('./api.js');
+
+angular.module('bitShip', []);
+
+angular
+  .module('bitShip')
+    .controller('bitShipOptionsController', bitShipOptionsController);
+
+function bitShipOptionsController($scope) {
+  console.log('util', util);
+  console.log('settings', util.storage.get());
+  $scope.settings = util.storage.get();
+}
